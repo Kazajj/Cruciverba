3154dea15f40a233c95ee2f40fa27cb2f3dab9af
Enzo Ilazaj <enzo.ilazay@eng.it>
2022-08-21 22:24:58 +0200

    Created Cruciverba

---
+++ src/cruciverba/CruciverbaMain.java
@@ -0,0 +1,16 @@
+package cruciverba;
+
+public class CruciverbaMain {
+
+    public static void main(String[] args) {
+//        int x = 20, y = 20, bombs = 50;
+//
+//        ModelGame modelGame = new ModelGame();
+//        Controller controller = new Controller(modelGame);
+//        GameListener listener = new ScreenView(controller);
+//        controller.setListener(listener);
+//
+//        controller.chooseGridSize(x, y, bombs);
+    }
+
+}
